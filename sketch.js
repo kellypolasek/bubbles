8683a54f5a8e8710c9fbdac1711452f8ac949d24
kellypolasek <fx2446@wayne.edu>
2016-02-11 17:52:44 -0500

    Bubbles-when-mouse-is-pressed-incomplete
    Tried to make bubbles appear when the mouse is pressed. Does not work;
    needs to be fixed.

--- sketch.js
+++ sketch.js
@@ -1,11 +1,26 @@
-var bubbles = [];
+var bubblepit = [];
 
-setup = function () {
-  createCanvas(600, 600);
-  // your code goes here
+//var myBubble = new Bubble(100, 100);
+//myBubble.speed.x = 2;
+//myBubble.speed.y = 2;
+
+var setup = function () {
+  createCanvas(600, 400);
+//  iterateWithForLoop(500);
 };
 
-draw = function () {
-  background(0);
-  // your code goes here
+var draw = function () {
+  background(100);
+  bubblepit.forEach(function updateAndDisplay(bubble) {
+    bubble.update();
+    bubble.display();
+    });
+    if (mouseIsPressed) bubblepit.push(new Bubble(mouseX, mouseY));
 };
+
+
+
+//var iterateWithForLoop = function (numberOfBubbles) {
+  //for (var i = 0; i < numberOfBubbles; ++i) {
+    //bubblepit.push (new Bubble (random(0, width-10), random(10, height - 10)));
+    //updateAndDisplay(array[i]);
