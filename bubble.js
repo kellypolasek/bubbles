7fd669d67ec4587433b3e8dc24da306a12bce066
kellypolasek <fx2446@wayne.edu>
2016-02-11 21:28:42 -0500

    Bubbles-That-Do-Not-Collect-At-Top
    Edited the code in bubbles.js so that the bubbles do not collect at the
    top but float continuously upward out of the frame.

--- bubble.js
+++ bubble.js
@@ -5,12 +5,12 @@ var Bubble = function (x, y) {
 
 Bubble.prototype = {
 
-  radius: 5,
+  radius: 10,
 
   update: function() {
     this.x += random(-1, 1);
     this.y += random(0, -2);
-    if (this.y - this.radius <= 0) this.y = this.radius + random(0, 2);
+
   },
 
   display: function () {
